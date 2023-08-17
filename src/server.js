6af45524700c279d6053f5118be397bc772a0e50
Archie Martin <98323890+archiemartini@users.noreply.github.com>
2023-08-17 09:12:27 +0100

    fixed dirname issue

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.get(/^(?!\/api).+/, (req, res) => {
-  res.sendFile(path.join(dirname, '../build/index.html'));
+  res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
 app.use(async (req, res, next) => {
